8890471bf998d0de894e548b585675a13c33fb02
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-01-27 14:53:37 +0530

    add dolby

--- common.mk
+++ common.mk
@@ -101,6 +101,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Dolby
+$(call inherit-product, hardware/dolby/dolby.mk)
+
 # Bootctrl
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
