dcf1fd82e0cee6bd5183605eb2e80bec0304835c
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-05-31 09:09:53 +0530

    sm6225: for voltage

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -107,7 +107,7 @@ TARGET_MODULE_ALIASES += \
     wlan.ko:qca_cld3_wlan.ko
 
 # Partitions
--include vendor/lineage/config/BoardConfigReservedSize.mk
+-include vendor/voltage/config/BoardConfigReservedSize.mk
 
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
 BOARD_USES_METADATA_PARTITION := true
